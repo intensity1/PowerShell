5ca69eac7a50bf820290b646943ee37620d4541e
@JoseEsposito_IT <jose@joseesposito.com>
2016-02-10 12:24:45 -0500

    Restart-PCs.ps1

---
+++ Restart-PCs.ps1
@@ -0,0 +1,16 @@
+﻿## Restart-Computer
+
+
+#get list of PCs
+$Computers = get-content C:\1\computers.csv
+
+#Restart them
+Restart-Computer $Computers -Verbose -force
+
+#wait 5 minutes
+
+Start-Sleep -s 300
+
+#ping computers to see if they respond
+
+Test-Connection -count 1 -computer $computers
